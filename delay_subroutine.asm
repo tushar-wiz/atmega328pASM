1d9c911ad7fab1f266a02e9ff3e7f865343f06ae
tushar <tushar.u110@gmail.com>
2022-02-09 00:23:15 +0530

    added Macro for delay

--- delay_subroutine.asm
+++ delay_subroutine.asm
@@ -4,9 +4,9 @@
 	; Created: 08 - 02 - 2022 11:39:44
 	; Author : tushar
 	;
-	
 	.NOLIST
 	.include "m328pdef.inc"
+	.include "include/delayMacro.inc"
 	.LIST
 	
 	.def mask = r16
@@ -29,8 +29,10 @@ START:
 	eor ledR, mask               ; Toggling LED
 	out PORTB, ledR              ;
 	
-	ldi loopCt, 100              ; setting loopCt value to get delay of 100 * 10ms = 1s
-	rcall DELAY10MS
+	;ldi loopCt, 100 ; setting loopCt value to get delay of 100 * 10ms = 1s
+	;rcall DELAY10MS
+	
+	delayMS 1000                 ; 1s delay
 	
 	rjmp START
 	
